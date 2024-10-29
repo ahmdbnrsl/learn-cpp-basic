1ad922b413252ffdf002cdc9e315ded3c072861c
unknown <ahmadbenirusli@gmail.com>
2024-10-29 21:12:58 +0700

    cpp: hello world and basic data type

---
+++ 01-hello.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // ini adalah library untuk output dan input
+
+int main()
+{
+    std::cout << "Hello, World!" << "Saya Cikarag" << std::endl; // ini adalah print pada console terminal
+    return 0;
+}
---
+++ 02-data-type.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    int integer = 10;
+    float decimal = 9.8;
+    double double_precission = 3.14;
+    std::string text = "Saya Cikarag";
+    bool isCikarag = false;
+    std::cout << " " << integer << " " << decimal << " " << double_precission << " " << text << " " << isCikarag << " " << std::endl;
+    return 0;
+}
